aa7937fa579bf7869cf033fbd69c228356419f79
tomon9086 <tomon9086@gmail.com>
2024-08-13 15:45:41 +0900

    read data/86-Elcentew.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let path = "data/86-Elcentew.txt";
+
+    if let Ok(file) = File::open(path) {
+        let reader = io::BufReader::new(file);
+
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                println!("{}", line);
+            }
+        }
+    }
+
+    Ok(())
 }
